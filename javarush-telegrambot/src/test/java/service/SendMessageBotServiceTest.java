46cd1b4dc1edff724120e4ae3d890814f6dde9c2
Ivanius <vanomelesh@gmail.com>
2023-10-29 13:22:26 +0300

    add testing for CommandContainer and SendMessageBotService

---
+++ javarush-telegrambot/src/test/java/service/SendMessageBotServiceTest.java
@@ -0,0 +1,38 @@
+package service;
+
+import bot.JavaRushTelegramBot;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@DisplayName("Unit-lvl testing for SendMessageBotService")
+public class SendMessageBotServiceTest {
+    private  SendMessageBotService sendMessageBotService;
+    private  JavaRushTelegramBot javaRushTelegramBot;
+
+    @BeforeEach
+    public void init(){
+        javaRushTelegramBot = Mockito.mock(JavaRushTelegramBot.class);
+        sendMessageBotService = new SendMessageBotServiceImp(javaRushTelegramBot);
+
+    }
+
+    @Test
+    public void shouldProperlySendMessage() throws TelegramApiException {
+        //given(подготавливаем все необходимое для теста)
+        String chatID = "chatID";
+        String message = "test message";
+
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatID);
+        sendMessage.enableHtml(true);
+        sendMessage.setText(message);
+        //when(запускаем метод, который планируем тестировать)
+        sendMessageBotService.sendMessage(chatID,message);
+        //then(проверяем, правильно ли отработал метод)
+        Mockito.verify(javaRushTelegramBot).execute(sendMessage); //Во время вызова метода verify() мы задали правило, что у объекта JRTB должен вызваться метод execute() с параметром sendMessage.
+    }
+}
